0d8ab341cc52d800405b3b1d5d261199684e2e82
Zakaria Kaoukab <Zkaoukab@omnidata.local>
2018-09-12 23:05:46 +0100

    initial commit

---
+++ src/main/java/com/cirb/archiver/ArchiverBatchApplication.java
@@ -0,0 +1,12 @@
+package com.cirb.archiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchiverBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchiverBatchApplication.class, args);
+	}
+}
